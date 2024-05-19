9dbc2f93bcbbf846293fe309464de2775f3cb918
farzad <87241134+farzad-eth@users.noreply.github.com>
2024-05-20 00:30:23 +0330

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-46a17afa-94fe-4674-9793-c44f24d4d4ed/"
 
 OUTPUT_DIR="logs"
 
